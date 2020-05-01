3383fdb7973186a636fbb50c66691b536cb3fdf8
DavidAmsellem <davidamsellemquiros@gmail.com>
2020-05-01 09:34:55 +0200

    Entrada de texto

--- test_input.py
+++ test_input.py
@@ -4,7 +4,8 @@ numero_del_isuario1 = int(input("dime un numero:"))
 numero_del_isuario2 = int(input("dime un numero:"))
 numero_del_isuario3 = int(input("dime un numero:"))
 
-resultado = max(numero_del_isuario1, numero_del_isuario2, numero_del_isuario3)
+resultado = float(max(numero_del_isuario1, numero_del_isuario2, numero_del_isuario3))
+
 
 print("el numero mas grande es {}".format(resultado))
 
