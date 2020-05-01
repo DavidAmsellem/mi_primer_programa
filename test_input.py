bcf9b6c483aba7f418ce060ed3037c2ce8d2688b
DavidAmsellem <davidamsellemquiros@gmail.com>
2020-05-01 09:25:54 +0200

    Entrada de texto

---
+++ test_input.py
@@ -0,0 +1,17 @@
+
+
+numero_del_isuario1 = int(input("dime un numero:"))
+numero_del_isuario2 = int(input("dime un numero:"))
+numero_del_isuario3 = int(input("dime un numero:"))
+
+resultado = max(numero_del_isuario1, numero_del_isuario2, numero_del_isuario3)
+
+print("el numero mas grande es {}".format(resultado))
+
+
+
+
+
+
+
+
