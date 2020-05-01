f44b19246b58c0f4639b5396497cb313618daba2
DavidAmsellem <davidamsellemquiros@gmail.com>
2020-05-01 09:51:58 +0200

    Entrada de texto

--- test_input.py
+++ test_input.py
@@ -6,12 +6,29 @@ numero_del_isuario3 = int(input("dime un numero:"))
 
 resultado = float(max(numero_del_isuario1, numero_del_isuario2, numero_del_isuario3))
 
+type(float(max(numero_del_isuario3, numero_del_isuario2, numero_del_isuario1)))
+
+
+
 
 print("el numero mas grande es {}".format(resultado))
 
 
+user_number = int(input("introduce un numero:"))
+
+if user_number >= 5:
+    print("el numero es mayor q 5")
 
 
+
+
+number_to_guess = 2
+user_number = int(input("adivina un numero; "))
+
+if user_number == number_to_guess:
+    print("has ganado")
+else:
+    print("has perdido")
 
 
 
